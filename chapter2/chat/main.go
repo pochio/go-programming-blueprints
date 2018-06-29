b428815501de69f2ac78794fab7e4cb6f8b88b4e
Hiroshi Senokuchi <h.sen.abm@gmail.com>
2018-06-29 21:42:29 +0900

    2.6 まで進んだ

---
+++ chapter2/chat/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+	"trace"
+
+	"github.com/stretchr/gomniauth"
+
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+)
+
+// temp1 は１つのテンプレートを表します
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	temp1    *template.Template
+}
+
+// ServeHTTP はHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.temp1.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() // フラグを解釈します
+	// Gomniauth のセットアップ
+	gomniauth.SetSecurityKey("セキュリティキー") // ランダム値
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密鍵", "http://localhost:8080/auth/callback/facebook"),
+		github.New("クライアントID", "秘密鍵", "http://localhost:8080/auth/callback/github"),
+		google.New("クライアントID", "秘密鍵", "http://localhost:8080/auth/callback/google"),
+	)
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+	// チャットルームを開始します
+	go r.run()
+	// Webサーバを起動します
+	log.Println("Webサーバを開始します。ポート: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
